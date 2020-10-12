37bce83c109f32a83db5839b6c6bdac7c1f302bb
Taia Dimitrova <taia.dimitrova@assist.ro>
2020-10-12 14:26:02 +0300

    updates

--- src/index.js
+++ src/index.js
@@ -1,45 +1,71 @@
-const WDIOReporter = require('@wdio/reporter').default
-const MapHooks = require('./mapHooks')
-const MapTests = require('./mapTests')
-const initResultSet = require('./initResultSet')
+const WDIOReporter = require("@wdio/reporter").default;
+const MapHooks = require("./mapHooks");
+const MapTests = require("./mapTests");
+const initResultSet = require("./initResultSet");
 
 class JsonReporter extends WDIOReporter {
-    constructor (options) {
-        options = Object.assign(options)
-        super(options)
+    constructor(options) {
+        options = Object.assign(options);
+        super(options);
     }
 
-    onRunnerEnd (runner) {
-        let json = this.prepareJson(runner)
-        this.write(JSON.stringify(json))
+    onRunnerEnd(runner) {
+        let json = this.prepareJson(runner);
+        this.write(JSON.stringify(json));
     }
 
-    prepareJson (runner) {
-        var resultSet = initResultSet(runner)
+    prepareJson(runner) {
+        var resultSet = initResultSet(runner);
 
         for (let specId of Object.keys(runner.specs)) {
-            resultSet.specs.push(runner.specs[specId])
+            resultSet.specs.push(runner.specs[specId]);
             for (let suiteKey of Object.keys(this.suites)) {
-                const suite = this.suites[suiteKey]
-                let testSuite = {}
-                testSuite.name = suite.title
-                testSuite.duration = suite._duration
-                testSuite.start = suite.start
-                testSuite.end = suite.end
-                testSuite.sessionId = runner.sessionId
-                testSuite.tests = MapTests(suite.tests)
-                testSuite.hooks = MapHooks(suite.hooks)
+                const suite = this.suites[suiteKey];
+                let alltests = [];
+                for (let testName of Object.keys(suite.tests)) {
+                    const test = suite.tests[testName];
+                    const testCase = {};
 
-                resultSet.state.failed += testSuite.hooks.filter(hook => hook.error).length
-                resultSet.state.passed += testSuite.tests.filter(test => test.state === 'passed').length
-                resultSet.state.failed += testSuite.tests.filter(test => test.state === 'failed').length
-                resultSet.state.skipped += testSuite.tests.filter(test => test.state === 'skipped').length
-                resultSet.suites.push(testSuite)
+                    testCase.testKey = runner.specs[specId]
+                        .split("\\")[3]
+                        .split(".")[0];
+                    testCase.name = test.title;
+                    testCase.start = test.start;
+                    testCase.end = test.end;
+                    testCase.duration = test._duration;
+                    let status;
+
+                    switch (test.state) {
+                        case "passed":
+                            status = "PASS";
+                            break;
+                        case "failed":
+                            status = "FAIL";
+                            break;
+                        default:
+                            status = "TODO";
+                            break;
+                    }
+
+                    testCase.status = status;
+
+                    if (test.error) {
+                        if (test.error.type) {
+                            testCase.comment = test.error.type;
+                        }
+                        if (test.error.message) {
+                            testCase.comment = test.error.message;
+                        }
+                    }
+
+                    alltests.push(testCase);
+                }
+                resultSet.tests = [...resultSet.tests, ...alltests];
             }
         }
 
-        return resultSet
+        return resultSet;
     }
 }
 
-exports.default = JsonReporter
+exports.default = JsonReporter;
