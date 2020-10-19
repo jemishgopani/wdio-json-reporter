ae3ed81f88459025c083cb72ce8859c9e815f799
TaiaDimitrova <68857744+TaiaDimitrova@users.noreply.github.com>
2020-10-19 15:47:29 +0300

    Fixed the split function, to get more accurate test id from test file name (#7)

--- src/index.js
+++ src/index.js
@@ -26,9 +26,9 @@ class JsonReporter extends WDIOReporter {
                     const test = suite.tests[testName];
                     const testCase = {};
 
-                    testCase.testKey = runner.specs[specId]
-                        .split("\\")[3]
-                        .split(".")[0];
+                    let specPathArray = runner.specs[specId].split("\\");
+                    let specPath = specPathArray[specPathArray.length - 1];
+                    testCase.testKey = specPath.split("_")[0];
                     testCase.name = test.title;
                     testCase.start = test.start;
                     testCase.end = test.end;
