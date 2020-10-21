0659e350d9234a93dda5041a4df08984c673e4f6
TaiaDimitrova <68857744+TaiaDimitrova@users.noreply.github.com>
2020-10-21 10:06:03 +0300

    changed data from results file (#11)

--- src/initResultSet.js
+++ src/initResultSet.js
@@ -1,12 +1,11 @@
 module.exports = function (runner) {
     let resultSet = {
+        testExecutionKey: "",
         info: {},
     };
 
-    resultSet.testExecutionKey = process.env.EXEKEY;
-    resultSet.info.start = runner.start;
-    resultSet.info.finish = runner.end;
-    resultSet.info.testEnvironments = process.env.DEVICE;
+    (resultSet.testExecutionKey = process.env.EXEKEY),
+        (resultSet.info.testEnvironments = [process.env.DEVICE]);
     resultSet.tests = [];
 
     return resultSet;
