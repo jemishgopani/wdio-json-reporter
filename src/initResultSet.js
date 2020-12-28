835838215232d4322fc0d4e89d61fb553856f55b
Taia Dimitrova <taia.dimitrova@assist.ro>
2020-12-28 15:11:21 +0200

    test result set object

--- src/initResultSet.js
+++ src/initResultSet.js
@@ -2,11 +2,11 @@ module.exports = function (runner) {
     let resultSet = {
         testExecutionKey: "",
         info: {},
+        tests: [],
     };
 
     resultSet.testExecutionKey = process.env.EXEKEY;
     resultSet.info.testEnvironments = [process.env.DEVICE];
-    resultSet.tests = [];
 
     return resultSet;
 };
