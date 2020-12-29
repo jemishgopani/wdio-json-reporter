8505947a43a1011bd48d396fa71ddc5d4af3e82a
TaiaDimitrova <68857744+TaiaDimitrova@users.noreply.github.com>
2020-12-29 09:58:38 +0200

    TEST| Test second (#27)
    * changed

--- src/index.js
+++ src/index.js
@@ -9,8 +9,24 @@ class JsonReporter extends WDIOReporter {
 
     onRunnerEnd(runner) {
         let json = this.prepareJson(runner);
-        let parsedJson = JSON.stringify(json);
-        this.write(JSON.parse(parsedJson));
+
+        console.log('BEFORE stringify: ', json)
+        console.log('AFTER stringify: ', JSON.stringify(json))
+
+        // let json = {
+        //     testExecutionKey: "CBC-9920",
+        //     info: {
+        //         testEnvironments: ["desktop"],
+        //     },
+        //     tests: [
+        //         {
+        //             testKey: "CBC-6965",
+        //             comment: 'chrome,SCENARIO: desktop | Gallery | Social Share Buttons | Verify Facebook button functionality(custom facebook popup) on richverse.com | 202 | qa-automation-post-01,STEP 7: Check that the Stay up to date popup is closing,Error:element (".qa-fb-like-popup__close-icon") still not existing after 10000ms',
+        //             status: "FAIL",
+        //         },
+        //     ],
+        // };
+        this.write(JSON.stringify(json));
     }
 
     prepareJson(runner) {
